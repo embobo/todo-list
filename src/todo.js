172e3e4ffd662d782623a9ce29c01f2a8792afa3
Emma <embobola@gmail.com>
2017-09-12 14:15:51 -0700

    add get_todos() function

--- src/todo.js
+++ src/todo.js
@@ -1,1 +1,12 @@
+function get_todos() {
+  var todos = new Array;
+  var todos_str = localStorage.getItem('todo');
+  if (todos_str !== null) {
+    todos = JSON.parse(todos_str);
+  }
+  return todos;
+}
 
+function add() {
+  
+}
